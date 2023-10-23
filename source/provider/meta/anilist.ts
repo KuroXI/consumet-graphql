e9827e4673e4f37ffc9293db5eacd195a6965b5c
KuroXI <vgonzales5157@gmail.com>
2023-10-23 11:34:46 +0800

    feat: added a random and airing schedule

--- source/provider/meta/anilist.ts
+++ source/provider/meta/anilist.ts
@@ -1,4 +1,6 @@
 import { axiosInstance } from "../../axios";
+import { AiringSchedule } from "../../types/Anilist";
+import "dotenv/config";
 
 export async function getInfo({ id }: { id: string }) {
   const { data } = await axiosInstance(`/meta/anilist/info/${id}`);
@@ -11,15 +13,46 @@ export async function getEpisodeLink({ id }: { id: string }) {
 }
 
 export async function getTrending({ page, perPage }) {
-  const { data } = await axiosInstance(
-    `/meta/anilist/trending?page=${page || 1}&perPage=${perPage || 20}`
-  );
+  const { data } = await axiosInstance("/meta/anilist/trending", {
+    params: {
+      page: page ?? 1,
+      perPage: perPage ?? 20,
+    },
+  });
   return data;
 }
 
 export async function getPopular({ page, perPage }) {
-  const { data } = await axiosInstance(
-    `/meta/anilist/popular?page=${page || 1}&perPage=${perPage || 20}`
-  );
+  const { data } = await axiosInstance("/meta/anilist/popular", {
+    params: {
+      page: page ?? 1,
+      perPage: perPage ?? 20,
+    },
+  });
+  return data;
+}
+
+export async function getRandom() {
+  const { data } = await axiosInstance(`/meta/anilist/random-anime`);
+  return data;
+}
+
+export async function getAiringSchedule({
+  page,
+  perPage,
+  weeekStart,
+  weekEnd,
+  notYetAired,
+}: AiringSchedule) {
+  const { data } = await axiosInstance("/meta/anilist/airing-schedule", {
+    params: {
+      page: page ?? 1,
+      perPage: perPage ?? 20,
+      weeekStart: weeekStart,
+      weeekEnd: weekEnd,
+      notYetAired: notYetAired ?? false,
+    },
+  });
+
   return data;
 }
